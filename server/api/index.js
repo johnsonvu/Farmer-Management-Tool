8363f4c1a1ada6f88f216c41cf51ef95bdd5e7e9
Justin Kwan <justinkkwan@alumni.ubc.ca>
2018-03-28 16:38:50 -0700

    updated gitignore

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 
 import users from './users'
 import animals from './animals'
+import farmers from './farmers'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
 router.use(animals)
+router.use(farmers)
 
 export default router
