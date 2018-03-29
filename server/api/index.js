58f43e0a71d3d6d16c2f77a4f678c26f79b8006d
Xingyu Tao <taoxingyu@gmail.com>
2018-03-28 17:26:37 -0700

    started implementing manage-animals, added get method for pens

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 
 import users from './users'
 import animals from './animals'
+import pens from './pens'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
 router.use(animals)
+router.use(pens)
 
 export default router
