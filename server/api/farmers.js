cc0245edb16564b409ba8564532c1b0034c6a0fb
Justin Kwan <justinkkwan@alumni.ubc.ca>
2018-03-29 11:33:23 -0700

    forgot server part

--- server/api/farmers.js
+++ server/api/farmers.js
@@ -29,6 +29,44 @@ router.get('/farmers/managers', function (req, res, next) {
         })
 })
 
+router.put('/farmers/workers', bodyParser.json(), function (req, res, next) {
+    const sin = req.body.data.sin
+    const firstname = req.body.data.firstname
+    const lastname = req.body.data.lastname
+    const manager_sin = req.body.data.manager_sin
+
+    const query = 'UPDATE Worker SET manager_sin = :manager_sin WHERE sin = :sin;'
+    const query2 = 'UPDATE Farmer SET firstname = :firstname, lastname = :lastname WHERE sin = :sin;'
+
+    connection.query(query,
+        {
+            type: connection.QueryTypes.UPDATE,
+            replacements: {
+                sin: sin,
+                manager_sin: manager_sin
+            }
+        })
+        .then(result => {
+            // result[1] is the number of rows changed
+            connection.query(query2,
+                {
+                    type: connection.QueryTypes.UPDATE,
+                    replacements: {
+                        sin: sin,
+                        firstname: firstname,
+                        lastname: lastname
+                    }
+                })
+                .then(result => {
+                    res.send('SUCCESS')
+            })
+        })
+        .catch(err => {
+            console.log('Error is: ')
+            console.log(err)
+        })
+})
+
 // router.post('/animals/feed', bodyParser.json(), function (req, res, next) {
 //     const date = req.body.data.date
 //     const food = req.body.data.food
