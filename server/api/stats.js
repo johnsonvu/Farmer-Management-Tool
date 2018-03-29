7643f02b5e2330e812e6c71711bc08b1d93e7797
johnsonvu <johnsonvu96@gmail.com>
2018-03-29 15:11:54 -0700

    Update stats page

--- server/api/stats.js
+++ server/api/stats.js
@@ -22,7 +22,7 @@ router.get('/stats/animal/performance/:option', function (req, res, next) {
             ON p.productid = w.productid
         ), production_avg AS (
             SELECT a.id, a.species, 
-            AVG(CASE WHEN ps.unit IS NULL THEN 0 ELSE ps.unit END)::int AS unit_avg
+            AVG(CASE WHEN ps.unit IS NULL THEN 0 ELSE ps.unit END)::DECIMAL(16,2) AS unit_avg
             FROM animal a
             LEFT JOIN production_units ps
             ON a.id = ps.animalid
@@ -32,6 +32,40 @@ router.get('/stats/animal/performance/:option', function (req, res, next) {
         SELECT species, ` + option + `(unit_avg) AS unit
         FROM production_avg 
         GROUP BY species`
+    connection.query(query, { type: connection.QueryTypes.SELECT })
+        .then(animals => {
+            console.log(animals)
+            res.json(animals)
+        })
+})
+
+router.get('/stats/animal/performance', function (req, res, next) {
+    const option = req.params.option
+    const query = `WITH production_units AS (
+            SELECT p.productid, p.productiondate, p.animalid, m.volume AS unit
+            FROM product p
+            INNER JOIN milk m
+            ON p.productid = m.productid
+            UNION
+            SELECT p.productid, p.productiondate, p.animalid, e.quantity AS unit
+            FROM product p
+            INNER JOIN egg e
+            ON p.productid = e.productid
+            UNION
+            SELECT p.productid, p.productiondate, p.animalid, w.weight AS unit
+            FROM product p
+            INNER JOIN wool w
+            ON p.productid = w.productid
+        ), production_avg AS (
+            SELECT a.id, a.species, 
+            AVG(CASE WHEN ps.unit IS NULL THEN 0 ELSE ps.unit END)::DECIMAL(16,2) AS unit_avg
+            FROM animal a
+            LEFT JOIN production_units ps
+            ON a.id = ps.animalid
+            GROUP BY a.id
+            ORDER BY species
+        )
+        SELECT * FROM production_avg pa JOIN animal a ON pa.id = a.id`
     connection.query(query, { type: connection.QueryTypes.SELECT })
         .then(animals => {
             console.log(animals)
